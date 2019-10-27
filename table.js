8d653a6f023873a7a2f0189d4280d1504a5745e0
nokkii <nokkii.03@gmail.com>
2019-10-27 11:14:43 +0900

    重複処理の関数化

--- table.js
+++ table.js
@@ -8,7 +8,7 @@ renderTable = function(){
   );
 
   var lineCount = Math.floor(menu2.length / 2);
-  var diff = 1;
+  var diff  = [1];
   var text1;
   var price1;
   var text2;
@@ -17,49 +17,43 @@ renderTable = function(){
   // 1行ずつ描画
   for( var i=0; i < lineCount; i++ )
   {
-    price1 = "";
-    price2 = "";
+    text1  = [""];
+    text2  = [""];
+    price1 = [""];
+    price2 = [""];
 
     // 一列目のテキストと値段を生成
-    if( typeof menu2[i] === 'string' )
-    {
-      diff--;
-
-      text1 = `${ menu2[i] }`;
-    } else {
-      text1 = `${ i+diff }.${ menu2[i].name }`;
-      if( typeof menu2[i].value.price === 'number' )
-      {
-        price1 = `${ menu2[i].value.price }円`;
-      } else {
-        Object.keys( menu2[i].value.price ).map( key => price1 += `${ key } ${ menu2[i].value.price[key] }円 ` );
-      }
-    }
-
+    buildMenuArray( text1, price1, i, 0, diff );
     // 2列めのテキストと値段を生成
-    if( typeof menu2[lineCount+i] === 'string' )
-    {
-      diff--;
-
-      text2 = `${menu2[ lineCount+i ]}`;
-    } else {
-      text2 = `${ lineCount+i-1 }.${ menu2[ lineCount+i ].name }`;
-      if( typeof menu2[lineCount+i-1].value.price === 'number' )
-      {
-        price2 = `${ menu2[lineCount+i-1].value.price }円`;
-      } else {
-        Object.keys( menu2[lineCount+i-1].value.price ).map( key => price2 += `${ key } ${ menu2[lineCount+i-1].value.price[key] }円 ` );
-      }
-    }
+    buildMenuArray( text2, price2, i, lineCount-1, null );
 
     // めくり用に40列40列のtrセルを描画
     lineHtml = "<tr>";
-    renderHalfCells( text1, price2, i, 0 );
+    renderHalfCells( text1[0], price2[0], i, 0 );
     // 真ん中仕切り
     lineHtml += `<td></td><td></td><td></td>`;
     // 右列描画
-    renderHalfCells( text2, price2, i, lineCount );
+    renderHalfCells( text2[0], price2[0], i, lineCount );
     $('#table').append( `${lineHtml}</tr>` );
+  }
+}
+
+buildMenuArray = function( text, price, i, lineCount, diff ){
+  if( typeof menu2[lineCount+i] === 'string' )
+  {
+    if( diff ){
+      diff[0]--;
+    }
+
+    text[0] = `${menu2[ lineCount+i ]}`;
+  } else {
+    text[0] = `${ diff ? lineCount+i+diff[0] : lineCount+i }.${ menu2[ lineCount+i ].name }`;
+    if( typeof menu2[lineCount+i].value.price === 'number' )
+    {
+      price[0] = `${ menu2[lineCount+i].value.price }円`;
+    } else {
+      Object.keys( menu2[lineCount+i].value.price ).map( key => price[0] += `${ key } ${ menu2[lineCount+i-1].value.price[key] }円 ` );
+    }
   }
 }
 
